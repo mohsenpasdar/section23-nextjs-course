df610232b4258c2a39c8a9f96f48445d9c4f8242
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2023-04-12 11:19:08 +0330

    12-linking btwn pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,8 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const NewsPage = () => {
     return (
-        <h1>the News Page</h1>
+        <>
+            <h1>the News Page</h1>
+            <ul>
+                <li><Link href='/news/1'>This is 1</Link></li>
+                <li><Link href='/news/2'>This is 2</Link></li>
+            </ul>
+        </>
     );
 };
 
