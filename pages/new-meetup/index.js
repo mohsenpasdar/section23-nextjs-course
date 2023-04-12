06846eca9b9e2c2dfd9ce21f041f9920fcad84fc
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2023-04-12 11:55:43 +0330

    16 ADDING THE NEWMEETUP FORM

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+const NewMeetupPage = () => {
+    const addMeetupHandler = (meetupData) => {
+        console.log(meetupData);
+    };
+    
+    return (
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />           
+    );
+};
+
+export default NewMeetupPage;
