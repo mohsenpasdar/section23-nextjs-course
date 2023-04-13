168e22681293930db69553dbdba7078c6eeb0cee
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2023-04-13 14:41:42 +0330

    28-Sending Http requests to API

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,8 +3,10 @@ import { MongoClient } from "mongodb";
 const handler = async (req, res) => {
     if (req.method === "POST") {
         const data = req.body;
+        console.log('backend', data);
 
         const client = await MongoClient.connect(process.env.MONGO_URI);
+        console.log(client);
         const db = client.db();
         const meetupsCollection = db.collection("meetups");
         const result = await meetupsCollection.insertOne(data);
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import { useRouter } from 'next/router';
 
 const NewMeetupPage = () => {
-    const addMeetupHandler = (meetupData) => {
-        console.log(meetupData);
+    const router = useRouter();
+
+    const addMeetupHandler = async (meetupData) => {
+        console.log('before fetching', meetupData);
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const data = await response.json();
+        console.log('after fetching', data);
+        
+        router.push('/');
     };
     
     return (
