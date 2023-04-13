88d3fccd49d723a9c8a8fcaed4424fee951635c6
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2023-04-13 12:00:50 +0330

    25 preparing paths with ...

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,4 +13,41 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Netflix-avatar.png/1200px-Netflix-avatar.png",
+        id: meetupId,
+        title: "meet up people",
+        address: "12345, city",
+        description: "An interesting meetup",
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
